138f61d6b48daca8e23073fa33e5d26bc5ad25d0
AnnaAD <anna.arpacidusseau@gmail.com>
2020-05-12 09:35:16 -0700

    :)

--- server/server.js
+++ server/server.js
@@ -33,7 +33,7 @@ const socket = require("./server-socket");
 
 // Server configuration below
 // TODO change connection URL after setting up your team database
-const mongoConnectionURL = "mongodb+srv://anna:114713@cluster0-yqqbj.mongodb.net/test?retryWrites=true&w=majority";
+const mongoConnectionURL = process.env.ATLAS_SRV;
 // TODO change database name to the name you chose
 const databaseName = "emonation";
 
@@ -57,7 +57,7 @@ app.use(express.json());
 // set up a session, which will persist login data across requests
 app.use(
   session({
-    secret: "session-secret",
+    secret: process.env.SECRET,
     resave: false,
     saveUninitialized: false,
   })
